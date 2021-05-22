b9649a37659a82613b2a58692cbeee4956db0425
Eric King <eric@webdeveric.com>
2021-05-22 00:54:17 -0500

    Added array of shortcut objects to check

--- src/contentScript.js
+++ src/contentScript.js
@@ -1,12 +1,39 @@
 'use strict';
 
+const eventIsShortcut = (event, shortcut) => {
+  const eventHasModifier = mod => event.getModifierState(mod);
+
+  if (shortcut.code === event.code && shortcut.modifier.some( eventHasModifier ) ) {
+    return (
+      shortcut.selector
+        ? event.originalTarget.matches(shortcut.selector)
+        : true
+    );
+  }
+
+  return false;
+};
+
+// TODO get these from browser storage
+const shortcuts = [
+  {
+    code: 'KeyK',
+    modifier: [ 'Control', 'Meta' ],
+  },
+  {
+    code: 'KeyL',
+    modifier: [ 'Control', 'Meta' ],
+  },
+];
+
+const shouldStop = event => {
+  return shortcuts.some( shortcut => eventIsShortcut(event, shortcut) );
+};
+
 document.addEventListener(
   'keydown',
   event => {
-    if (
-      event.code === 'KeyK' &&
-      (event.getModifierState('Control') || event.getModifierState('Meta'))
-    ) {
+    if (shouldStop(event)) {
       event.stopPropagation();
     }
   },
@@ -15,4 +42,3 @@ document.addEventListener(
     passive: true,
   },
 );
-
