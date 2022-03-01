4036021fa25e3abce8b257ecacf9919c3c1318cf
Eric King <eric@webdeveric.com>
2022-03-01 17:06:50 -0600

    Linting updates

--- src/util/eventIsShortcut.ts
+++ src/util/eventIsShortcut.ts
@@ -1,17 +1,17 @@
 import { createShortcutWithDefaults } from './createShortcutWithDefaults';
 import type { Shortcut } from '../models/shortcut';
 
-export const eventIsShortcut = (event: KeyboardEvent, shortcut: Shortcut): boolean => {
-  const s = createShortcutWithDefaults(shortcut);
+export const eventIsShortcut = (event: KeyboardEvent, shortcutInput: Shortcut): boolean => {
+  const shortcut = createShortcutWithDefaults(shortcutInput);
 
   if (
-    s.altKey === event.altKey &&
-    s.ctrlKey === event.ctrlKey &&
-    s.metaKey === event.metaKey &&
-    s.shiftKey === event.shiftKey &&
-    s.key === event.key.toLocaleLowerCase()
+    shortcut.altKey === event.altKey &&
+    shortcut.ctrlKey === event.ctrlKey &&
+    shortcut.metaKey === event.metaKey &&
+    shortcut.shiftKey === event.shiftKey &&
+    shortcut.key === event.key.toLocaleLowerCase()
   ) {
-    return s.selector ? !!event.target && (event.target as Element).matches(s.selector) : true;
+    return shortcut.selector ? !!event.target && (event.target as Element).matches(shortcut.selector) : true;
   }
 
   return false;
--- src/util/getUniqueShortcuts.ts
+++ src/util/getUniqueShortcuts.ts
@@ -1,7 +1,7 @@
 import { Shortcut } from '../models/shortcut';
 
 export const getUniqueShortcuts = (shortcuts: Shortcut[]): Shortcut[] => {
-  const items = new Set(shortcuts.map(s => JSON.stringify(s, null, 0)));
+  const items = new Set(shortcuts.map(shortcut => JSON.stringify(shortcut, null, 0)));
 
   return [...items].map(item => JSON.parse(item));
 };
