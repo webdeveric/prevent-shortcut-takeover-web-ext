c11b4e871e55462dc3880436126ad50a6d176d67
Eric King <eric@webdeveric.com>
2025-03-23 14:30:52 -0500

    refactor: use implicit return

--- src/utils/getUniqueShortcuts.ts
+++ src/utils/getUniqueShortcuts.ts
@@ -1,7 +1,4 @@
 import type { Shortcut } from '@models/shortcut.js';
 
-export const getUniqueShortcuts = (shortcuts: Shortcut[]): Shortcut[] => {
-  const items = new Set(shortcuts.map(shortcut => JSON.stringify(shortcut, null, 0)));
-
-  return [...items].map(item => JSON.parse(item));
-};
+export const getUniqueShortcuts = (shortcuts: Shortcut[]): Shortcut[] =>
+  [...new Set(shortcuts.map((shortcut) => JSON.stringify(shortcut, null, 0)))].map((item) => JSON.parse(item));
