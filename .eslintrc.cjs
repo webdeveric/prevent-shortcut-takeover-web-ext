cdf9c95d48a58eea3cc58b45b0ae61d831879708
Eric King <eric@webdeveric.com>
2026-03-12 18:25:25 -0500

    chore(linting): added comment

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-require-imports
 const tsconfig = require('./tsconfig.json');
 
 const internalPathAliases = Object.keys(tsconfig.compilerOptions.paths)
