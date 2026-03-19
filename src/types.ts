b5cbebe2df1e74458a7304a58f070dcada856860
Eric King <eric@webdeveric.com>
2026-03-19 16:26:27 -0500

    feat: added `ActiveTabChanged` type

--- src/types.ts
+++ src/types.ts
@@ -8,3 +8,15 @@ export type WithPropertyValue<Input, Value> = {
 export type ThemeColors = Pretty<WithPropertyValue<NonNullable<Manifest.ThemeType['colors']>, null>>;
 
 export type ThemeColor = NonNullable<ThemeColors[keyof ThemeColors]>;
+
+export type ActiveTabChanged = {
+  type: 'ACTIVE_TAB_CHANGED';
+  payload: {
+    tabId: number;
+    url: string | undefined;
+    favIconUrl: string | undefined;
+    title: string | undefined;
+  };
+};
+
+export type ExtensionMessage = ActiveTabChanged;
