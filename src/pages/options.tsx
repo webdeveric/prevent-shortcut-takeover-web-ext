d3859829b4fe87089c7f28d86fc4cd3e117643cd
Eric King <eric@webdeveric.com>
2026-04-13 11:59:28 -0500

    refactor: be more strict with  `render()`

--- src/pages/options.tsx
+++ src/pages/options.tsx
@@ -1,7 +1,16 @@
 import { createRoot } from 'react-dom/client';
 
+import { Bootstrap } from '@components/Bootstrap.js';
 import { OptionsPage } from '@components/OptionsPage/OptionsPage.jsx';
 
-const root = createRoot(document.getElementById('root') as HTMLElement);
+const root = document.getElementById('root');
 
-root.render(<OptionsPage />);
+if (!root) {
+  throw new Error('Root element not found');
+}
+
+createRoot(root).render(
+  <Bootstrap>
+    <OptionsPage />
+  </Bootstrap>,
+);
