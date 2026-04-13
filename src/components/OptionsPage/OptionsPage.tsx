ef99a66fdfd788df80bff004eea83b8ec5bf71ce
Eric King <eric@webdeveric.com>
2026-04-13 12:08:01 -0500

    refactor: separated `ShortcutList` from the form state

--- src/components/OptionsPage/OptionsPage.tsx
+++ src/components/OptionsPage/OptionsPage.tsx
@@ -1,136 +1,45 @@
-import { useCallback, useState, type ChangeEvent, type ReactElement, type SubmitEvent } from 'react';
+import { BrowserColorPalette } from '@components/BrowserColorPalette.js';
+import { AddShortcutForm } from '@components/OptionsPage/AddShortcutForm.js';
+import { ShortcutList } from '@components/ShortcutList/ShortcutList.js';
 
-import { Bootstrap } from '@components/Bootstrap.jsx';
-import { ShortcutInput } from '@components/ShortcutInput.jsx';
-import { ShortcutList } from '@components/ShortcutList/ShortcutList.jsx';
-import { useBrowserStorage } from '@hooks/useBrowserStorage.js';
-import { BrowserStorageKey, StorageAreaName } from '@models/storage.js';
-import { formatShortcut } from '@utils/formatShortcut.js';
-import { getUniqueShortcuts } from '@utils/getUniqueShortcuts.js';
-import { isValidSelector } from '@utils/isValidSelector.js';
-import { shortcutsEqual } from '@utils/shortcutsEqual.js';
-import { assertIsShortcutArray } from '@utils/type-assertion.js';
-import type { Shortcut } from '@models/shortcut.js';
+import './OptionsPage.css';
 
-import * as styles from './OptionsPage.css';
+import type { ReactElement } from 'react';
 
 export const OptionsPage = (): ReactElement => {
-  const { value, loading, error, set } = useBrowserStorage<Shortcut[]>(
-    BrowserStorageKey.Shortcuts,
-    StorageAreaName.Local,
-    [], // No shortcuts defined by default
-    assertIsShortcutArray,
-  );
-  const [newShortcut, setNewShortcut] = useState<Shortcut | undefined>();
-  const [newSelector, setNewSelector] = useState<string>();
-  const [errorMessage, setErrorMessage] = useState<string>();
-
-  const addShortcut = useCallback(
-    (event: SubmitEvent<HTMLFormElement>) => {
-      event.preventDefault();
-
-      if (newShortcut) {
-        if (isValidSelector(newSelector)) {
-          newShortcut.selector = newSelector;
-
-          set(getUniqueShortcuts([...(value ?? []), newShortcut]))
-            .then((): void => {
-              setNewShortcut(undefined);
-              setErrorMessage(undefined);
-            })
-            .catch((storageError): void => {
-              setErrorMessage(String(storageError));
-            });
-        } else {
-          setErrorMessage('CSS selector is invalid');
-        }
-      }
-    },
-    [newShortcut, newSelector, set, value],
-  );
-
-  const onSelectorChange = useCallback((event: ChangeEvent<HTMLInputElement>) => {
-    setNewSelector(event.target.value);
-    setErrorMessage(undefined);
-  }, []);
-
-  const removeShortcut = useCallback(
-    (shortcut: Shortcut): void => {
-      const shortcuts = (value ?? []).filter((item) => !shortcutsEqual(item, shortcut));
-
-      set(shortcuts).catch(console.error);
-    },
-    [set, value],
-  );
-
   return (
-    <Bootstrap>
+    <>
       <p>The shortcuts listed below will not be allowed to be taken over by websites.</p>
 
       <h2>Shortcuts</h2>
 
-      {error && <p className={styles.errorMessage}>{error.message}</p>}
+      <ShortcutList className="shortcutList" />
 
-      {!loading && value && (
-        <ShortcutList className={styles.shortcutList} shortcuts={value} removeShortcut={removeShortcut} />
-      )}
+      <AddShortcutForm />
 
-      <details className={styles.details}>
-        <summary>
-          <h2>Add shortcut</h2>
-        </summary>
-
-        <p>
-          <strong>Keyboard shortcut:</strong> Click in the box then perform a shortcut, such as{' '}
-          <strong>ctrl + k</strong>.
-        </p>
-
-        <p>
-          <strong>CSS Selector:</strong> If the <code>event.target</code> matches the selector, the shortcut will not be
-          allowed to be taken over. This can be useful if you don&apos;t want to prevent a shortcut for the entire{' '}
-          <code>document</code>.{' '}
-          <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Selectors">
-            Read the CSS selector documentation
+      {process.env['NODE_ENV'] === 'development' && (
+        <footer className="border-browser-popup-border popup-footer mt-4 flex flex-row flex-nowrap content-center items-center justify-center gap-3 rounded-sm border p-3">
+          <a href={window.location.href} target="_blank" rel="noreferrer" title="Open action popup URL in a new tab">
+            This page
           </a>
-          .
-        </p>
-      </details>
 
-      <form onSubmit={addShortcut} className={styles.form}>
-        <div className={styles.inputItems}>
-          <div>
-            <label className={styles.label} htmlFor="shortcut-input">
-              Keyboard shortcut
-            </label>
-            <ShortcutInput
-              className={styles.input}
-              id="shortcut-input"
-              tabIndex={1}
-              setShortcut={setNewShortcut}
-              value={formatShortcut(newShortcut)}
-            />
-          </div>
+          <a href={window.location.href} target="_blank" rel="noreferrer" title="Open action popup URL in a new tab">
+            This page
+          </a>
 
-          <div>
-            <label className={styles.label} htmlFor="css-selector-input">
-              CSS selector (optional)
-            </label>
-            <input
-              id="css-selector-input"
-              className={styles.input}
-              tabIndex={2}
-              type="text"
-              onChange={onSelectorChange}
-            />
-          </div>
-        </div>
+          <a
+            className="cursor-pointer"
+            title="Click to throw an error for testing the error boundary"
+            onClick={() => {
+              throw new Error('Testing');
+            }}
+          >
+            Throw <code>Error()</code>
+          </a>
 
-        {errorMessage && <p className={styles.errorMessage}>{errorMessage}</p>}
-
-        <button type="submit" className={styles.submitButton} tabIndex={3} disabled={!newShortcut || !!errorMessage}>
-          Add shortcut
-        </button>
-      </form>
-    </Bootstrap>
+          <BrowserColorPalette />
+        </footer>
+      )}
+    </>
   );
 };
