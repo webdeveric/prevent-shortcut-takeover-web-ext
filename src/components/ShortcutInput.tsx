3d0e21ca4373f43e7b870d7e14c6fc9ca56f1298
Eric King <eric@webdeveric.com>
2026-04-13 12:03:22 -0500

    refactor: use `<Input>` component

--- src/components/ShortcutInput.tsx
+++ src/components/ShortcutInput.tsx
@@ -1,59 +1,42 @@
-import {
-  useCallback,
-  type ChangeEventHandler,
-  type FunctionComponent,
-  type InputHTMLAttributes,
-  type KeyboardEvent,
-} from 'react';
+import { useCallback, useState, type ChangeEventHandler, type FunctionComponent, type KeyboardEvent } from 'react';
 
+import { Input, type InputProps } from '@components/Input.js';
 import { metaKeyPattern } from '@src/constants.js';
+import { formatShortcut } from '@utils/formatShortcut.js';
 import { getShortcutFromEvent } from '@utils/getShortcutFromEvent.js';
 import { hasModifier } from '@utils/hasModifier.js';
 import { isFunctionKey } from '@utils/type-predicate.js';
-import type { Shortcut } from '@models/shortcut.js';
 
-export interface ShortcutInputProps extends InputHTMLAttributes<HTMLInputElement> {
-  setShortcut: (shortcut?: Shortcut) => void;
-}
+export type ShortcutInputProps = Omit<
+  InputProps,
+  'value' | 'type' | 'onChange' | 'onKeyDown' | 'onKeyDownCapture' | 'children'
+>;
 
 const doNothing: ChangeEventHandler<HTMLInputElement> = (event): void => event.preventDefault();
 
-export const ShortcutInput: FunctionComponent<ShortcutInputProps> = ({
-  setShortcut,
-  ...inputProps
-}: ShortcutInputProps) => {
-  const onKeyDown = useCallback(
-    (event: KeyboardEvent) => {
-      const hasModifierKey = hasModifier(event);
+export const ShortcutInput: FunctionComponent<ShortcutInputProps> = (props) => {
+  const [value, setValue] = useState('');
 
-      // Clear the shortcut if Backspace is pressed without any modifier keys
-      if (!hasModifierKey && event.key === 'Backspace') {
-        setShortcut(undefined);
+  const onKeyDownCapture = useCallback((event: KeyboardEvent) => {
+    const hasModifierKey = hasModifier(event);
 
-        return;
-      }
+    // Clear the shortcut if Backspace is pressed without any modifier keys
+    if (!hasModifierKey && event.key === 'Backspace') {
+      return setValue('');
+    }
 
-      // Only set the shortcut if a non-modifier key is pressed along with at least one modifier key,
-      // or if a function key (F1-F12) is pressed (modifiers optional)
-      if (!metaKeyPattern.test(event.key) && (hasModifierKey || isFunctionKey(event.key))) {
-        setShortcut(getShortcutFromEvent(event));
-      }
+    // Only set the shortcut if a non-modifier key is pressed along with at least one modifier key,
+    // or if a function key (F1-F12) is pressed (modifiers optional)
+    if (!metaKeyPattern.test(event.key) && (hasModifierKey || isFunctionKey(event.key))) {
+      setValue(formatShortcut(getShortcutFromEvent(event)));
+    }
 
-      if (event.key !== 'Tab' && event.key !== 'Enter') {
-        event.preventDefault();
-      }
-    },
-    [setShortcut],
-  );
+    if (!hasModifierKey && (event.key === 'Tab' || event.key === 'Enter')) {
+      return;
+    }
 
-  return (
-    <input
-      type="text"
-      {...inputProps}
-      onKeyDownCapture={onKeyDown}
-      onChange={doNothing}
-      autoComplete="off"
-      spellCheck="false"
-    />
-  );
+    event.preventDefault();
+  }, []);
+
+  return <Input type="text" onKeyDownCapture={onKeyDownCapture} onChange={doNothing} value={value} {...props} />;
 };
